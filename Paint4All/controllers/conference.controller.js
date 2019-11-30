96e912a478050ddf7d5b73dc8adf542a9b6c7e8c
Lucasf15 <llffabc@gmail.com>
2019-11-30 14:56:56 +0000

    Delete duplicated files

--- Paint4All/controllers/conference.controller.js
+++
@@ -1,5 +0,0 @@
-//Connect database
-const connect = require('../config/connectMySQL');
-//Include JSON folder
-const jsonMessagesPath = __dirname + "/../assets/jsonMessages/";
-const jsonMessages = require(jsonMessagesPath + "bd");
